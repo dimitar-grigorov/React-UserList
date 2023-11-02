531ff1d1810ac5a6fcee0526817b4849ec49758f
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-11-02 19:18:43 +0200

    Add initial files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    <footer className="footer">
+        <p>This site is designed to be used htmlFor training purposes at SoftUni.</p>
+    </footer>
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    <header className="header">
+        <div className="logo">
+            <span className="course">React Workshop - September 2023</span>
+        </div>
+    </header>
+};
+
+export default Header;
